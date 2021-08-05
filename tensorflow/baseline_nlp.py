4d9f3198a177a35f8a21d5805e4b1ed88830196a
lucasponteslpa <lpa@cin.ufpe.br>
2021-08-05 09:48:56 -0300

    Update baseline with digits.

--- tensorflow/baseline_nlp.py
+++ tensorflow/baseline_nlp.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 from sklearn import preprocessing, decomposition
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_digits
 import os
 import numpy as np
 import sys
@@ -30,10 +32,13 @@ def pad(data):
 use_pad = False
 if len(sys.argv)==2:
     if sys.argv[1] == 'cifar10':
-        data_train = np.load('../cifar10_mobilenetv2_features/train/data.npy')
-        labels_train = np.load('../cifar10_mobilenetv2_features/train/labels.npy')
-        data_val = np.load('../cifar10_mobilenetv2_features/val/data.npy')
-        labels_val = np.load('../cifar10_mobilenetv2_features/val/labels.npy')
+        data_train = np.load('../cifar10_mobilenetv2_features_2class/train/data.npy')
+        labels_train = np.load('../cifar10_mobilenetv2_features_2class/train/labels.npy')
+        data_val = np.load('../cifar10_mobilenetv2_features_2class/val/data.npy')
+        labels_val = np.load('../cifar10_mobilenetv2_features_2class/val/labels.npy')
+    elif sys.argv[1]=='digits':
+        data, labels = load_digits(n_class=10, return_X_y=True)
+        data_train, data_val, labels_train, labels_val = train_test_split(data, labels, test_size=0.3)
     else:
         data_train = np.load('../mnist_features/train/data.npy')
         labels_train = np.load('../mnist_features/train/labels.npy')
@@ -49,41 +54,50 @@ else:
 if use_pad:
     data_train = pad(data_train)
     data_val = pad(data_val)
-n_class = 10
-pca = decomposition.PCA(n_components=512)
+n_class = 2
+pca = decomposition.PCA(n_components=128)
 labels_train = load_hot_labels(labels_train, n_class)
 labels_val = load_hot_labels(labels_val, n_class)
-res_dir = 'results_baseline_nlp'
-make_dirs(res_dir)
 arch_type = 3
+res_dir = 'results_baseline_cifar10_2classes_pca_arch3'
+make_dirs(res_dir)
 if arch_type == 0:
-    model = tf.keras.Sequential([tf.keras.layers.Dense(n_class,activation="sigmoid")])
+    model = tf.keras.Sequential([tf.keras.layers.Dense(n_class,activation="softmax")])
 elif arch_type == 1:
     model = tf.keras.Sequential([tf.keras.layers.Dense(1,activation="relu"),
-                                 tf.keras.layers.Dense(n_class,activation="sigmoid")])
+                                 tf.keras.layers.Dense(n_class,activation="softmax")])
 elif arch_type == 2:
     model = tf.keras.Sequential([tf.keras.layers.Dense(2,activation="relu"),
-                                 tf.keras.layers.Dense(n_class,activation="sigmoid")])
+                                 tf.keras.layers.Dense(n_class,activation="softmax")])
 elif arch_type == 3:
-    model = tf.keras.Sequential([tf.keras.layers.Conv1D(1, 64, strides=64, activation="relu"),
+    model = tf.keras.Sequential([tf.keras.layers.Conv1D(2, 8, strides=6, activation="relu"),
                                  tf.keras.layers.Flatten(),
-                                 tf.keras.layers.Dense(n_class,activation="sigmoid")])
+                                 tf.keras.layers.Dense(n_class,activation="softmax")])
 optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
 model.compile(optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
-#pca_data = pca.fit_transform(np.concatenate((data_train, data_val)))
-if arch_type == 3:
-    pca_data = np.expand_dims(np.concatenate((data_train[:5000,:512], data_val[:1500,:512])),axis=2)
+if sys.argv[1]!='digits':
+    if arch_type == 3:
+        #pca_data = np.expand_dims(np.concatenate((data_train[:5000,:512], data_val[:1500,:512])),axis=2)
+        pca_data = np.expand_dims(np.concatenate((data_train[:5000], data_val[:1500])),axis=2)
+    else:
+        pca_data = np.concatenate((data_train[:5000], data_val[:1500]))
 else:
-    pca_data = np.concatenate((data_train[:5000,:512], data_val[:1500,:512]))
+    if arch_type == 3:
+        pca_data = np.expand_dims(np.concatenate((data_train, data_val)),axis=2)
+    else:
+        pca_data = np.concatenate((data_train, data_val))
+pca_data = pca.fit_transform(pca_data)
+#pca_data = np.expand_dims(pca_data, axis=2)
 #scaler = preprocessing.StandardScaler().fit(pca_data)
 #pca_data = scaler.transform(pca_data)
 #pca_data = preprocessing.normalize(pca_data, norm='l2')
-train = model.fit(x=pca_data[:5000],
-                  y=labels_train[:5000],
-                  epochs=5,
+train = model.fit(x=pca_data[:5000] if sys.argv[1] != 'digits' else pca_data[:data_train.shape[0]],
+                  y=labels_train[:5000] if sys.argv[1] != 'digits' else labels_train,
+                  epochs=50,
                   batch_size=16,
                   verbose=1,
-                  validation_data=(pca_data[5000:],labels_val[:1500]))
+                  validation_data=(pca_data[5000:] if sys.argv[1] != 'digits' else pca_data[data_train.shape[0]:],
+                                   labels_val[:1500] if sys.argv[1] != 'digits' else labels_val))
 print(model.summary())
 np.save(os.path.join(res_dir,'train_loss'),train.history['loss'])
 np.save(os.path.join(res_dir,'train_acc'),train.history['accuracy'])
